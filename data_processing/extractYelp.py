ac18e4ce0f9680d08d8c4d2afddf6fe1b92135d7
Saad Lahlou <saad@MacBook-Pro-de-Saad.local>
2017-12-02 18:31:55 -0500

    Loading Yelp dataset, with balanced ratings and only english reviews

--- data_processing/extractYelp.py
+++ data_processing/extractYelp.py
@@ -8,12 +8,15 @@ Created on Wed Nov 29 16:31:49 2017
 
 import numpy as np 
 import os
+import json
 from os import listdir
 from os.path import isfile, join
 from sklearn.model_selection import train_test_split
 from numpy import random
 import torch
 import re
+from langdetect import detect
+import random
 
 
 alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -22,7 +25,7 @@ alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             '[',']', '{', '}','"']
 
 indexing = { letter : i+1 for i, letter in enumerate(alphabet)}
-indexing['UNK'] = len(alphabet)
+indexing['UNK'] = len(alphabet)+1
 indexing['No_letter'] = 0
 bigAlphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 p=len(indexing)
@@ -31,50 +34,72 @@ altIndexing = { letter : i+p+1 for i,letter in enumerate(bigAlphabet)}
 bigIndexing.update(indexing)
 altIndexing.update(indexing)
 
-def createFiles(file_name='review.txt', batch_size=0,total_size=398000,train_proportion=360/398): 
+
+def shuffle(): 
+    with open('review.txt','r') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open('review_shuffle.txt','w') as target:
+        for _, line in data:
+            target.write( line )
+
+def createFiles(file_name='review_shuffle.txt', batch_size=0,total_size=398000,train_proportion=360/398): 
     file_object = open(file_name,"r")
     counter =0
     counter_pos=0
     counter_neg=0
     train_size=(int)(train_proportion*total_size)
     test_size= total_size - train_size
+    print(test_size)
+    print(train_size)
+    
     file_train=open("train.txt","w")
     file_test=open("test.txt","w")
 
     with open(file_name) as infile:
         while(counter<train_size):
             text = infile.readline()
+            
             if len(text)==0: 
+                print('break')
                 break
-            liste = re.split('"stars":',text)
-            rating = float(re.split(',',liste[1])[0])
-            
-            review = re.split('"text":',liste[1])[1]
             
             
-            binary =1
+            info=json.loads(text)
+            rating = info['stars']
+            review = info['text']
+            item={}
+            item['review']=review
+            binary=int(rating>3)
+            item['rating']=binary
             
-            if(rating<4): 
-                binary=0
+            
+            try:
+                language = detect(review)
+                
+            except: 
+                print('failure to detect language')
+                continue
+            
+            if(language!='en'):
+                print(detect(review))
+                continue
             
             if(binary==0 and counter_neg<train_size/2):
-                file_train.write('reviewInfo: ')
-                file_train.write(review)
-                file_train.write('ratingInfo: ')
-                file_train.write(str(rating))
+                json.dump(item,file_train)
                 file_train.write('\n')
                 counter_neg+=1
                 counter+=1
+                print(counter)
                 
-            if(binary==1 and counter_pos < train_size/2):
-                file_train.write('reviewInfo: ')
-                file_train.write(review)
-                file_train.write('ratingInfo: ')
-                file_train.write(str(rating))
+            elif(binary==1 and counter_pos < train_size/2):
+                json.dump(item,file_train)
                 file_train.write('\n')
                 counter_pos+=1
                 counter+=1
+                print(counter)
                 
+        file_train.close()       
         counter=0
         counter_neg=0
         counter_pos=0
@@ -82,42 +107,38 @@ def createFiles(file_name='review.txt', batch_size=0,total_size=398000,train_pro
         while(counter<test_size):
             text = infile.readline()
             if len(text)==0: 
+                print('break')
                 break
+            info=json.loads(text)
+            rating = info["stars"]
+            review = info["text"]
+            item={}
+            item["review"]=review
+            binary=int(rating<4)
+            item["rating"]=binary
             
-            liste = re.split('"stars":',text)
-            rating = float(re.split(',',liste[1])[0])
-            
-            review = re.split('"text":',liste[1])[1]
-            
-            binary =1
-            
-            if(rating<4): 
-                binary=0
-            
+            if(detect(review)!='en'):
+                print(detect(review))
+                continue
             
             if(binary==0 and counter_neg<test_size/2):
-                file_test.write('reviewInfo: ')
-                file_test.write(review)
-                file_test.write('ratingInfo: ')
-                file_test.write(str(rating))
+                json.dump(item,file_test)
                 file_test.write('\n')
                 counter_neg+=1
                 counter+=1
                 
             if(binary==1 and counter_pos < test_size/2):
-                file_test.write('reviewInfo: ')
-                file_test.write(review)
-                file_test.write('ratingInfo: ')
-                file_test.write(str(rating))
+                json.dump(item,file_test)
                 file_test.write('\n')
                 counter_pos+=1
                 counter+=1
                 
                 
+        file_test.close()
                 
                 
                 
-def dataGenerator(file_name='train.txt',train_split=0.8,binary=False, max_length=1014, indexing_choice=0):
+def dataGenerator(file_name, test=False,train_split=0.8, max_length=1014, indexing_choice=0):
     
     if(indexing_choice==0):
         index = indexing
@@ -130,26 +151,17 @@ def dataGenerator(file_name='train.txt',train_split=0.8,binary=False, max_length
     dataset = []
     with open(file_name) as infile:
         while True: 
-            rev=infile.readline() 
-            if len(rev)==0:
+            text=infile.readline() 
+            counter+=1
+            if len(text)==0:
                 break
             
-            data = re.split('reviewInfo: ',rev)[1]
+            info=json.loads(text)
+            rating = info["rating"]
+            data = info["review"]
             
-            rev=infile.readline()
-            
-            rating = float(re.split('ratingInfo:',rev)[1])
-            
-            if(binary):
-                if(rating<4):
-                    rating=0
-                else:
-                    rating=1
-            else:
-                rating= int(rating) -1
-                
+        
             review = torch.zeros(max_length).long()
-            
             
             for i in range(min(max_length,len(data))):
                 letter = data[i].lower()
@@ -160,60 +172,14 @@ def dataGenerator(file_name='train.txt',train_split=0.8,binary=False, max_length
                     review[i] = index['UNK']
         
             dataset.append({'review': review, 'rating':torch.IntTensor([rating])})
-        
+            
     #random split 0.8 / 0.2
-    dataset_train, dataset_val =  train_test_split(dataset, test_size=1-train_split)
+    if test:
+        return dataset
+    else  :
+        dataset_train, dataset_val =  train_test_split(dataset, test_size=1-train_split)
 
-    return dataset_train, dataset_val
-
-
-def dataGeneratorTest (file_name='test.txt',binary=False, max_length=1014, indexing_choice=0):
-    
-    if(indexing_choice==0):
-        index = indexing
-    if(indexing_choice==1):
-        index = bigIndexing
-    if(indexing_choice==2):
-        index = altIndexing
-        
-    dataset = []
-    with open(file_name) as infile:
-        while True: 
-            rev=infile.readline() 
-            
-            if len(rev)==0:
-                break
-                
-            data = re.split('reviewInfo: ',rev)[1]
-            
-            rev=infile.readline()
-            
-            rating = float(re.split('ratingInfo:',rev)[1])
-            
-            if(binary):
-                if(rating<4):
-                    rating=0
-                else:
-                    rating=1
-            else:
-                rating= int(rating) -1
-                
-            review = torch.zeros(max_length).long()
-            
-            
-            for i in range(min(max_length,len(data))):
-                letter = data[i].lower()
-                
-                if letter in alphabet:
-                    review[i] = index[letter]
-                else:
-                    review[i] = index['UNK']
-
-            dataset.append({'review': review, 'rating':torch.IntTensor([rating])})
-        
-
-    return dataset
-
+        return dataset_train, dataset_val
 
 
 
@@ -255,10 +221,11 @@ def eval_iter(source, batch_size):
 
 
 
-if __name__ == '__main__':
-    createFiles()
-    training_set, validation_set = dataGenerator()
-    test_set = dataGeneratorTest()
+if __name__ == '__main__': 
+    #shuffle()
+    #createFiles()
+    training_set, validation_set = dataGenerator('train.txt')
+    test_set = dataGenerator('test.txt',test=True)
     print("length training_set : ", len(training_set))
     print("length validation_set : ", len(validation_set))
     print("length test_set : ", len(test_set))
